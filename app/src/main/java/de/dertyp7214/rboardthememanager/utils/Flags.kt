65efcf6bf21905d277d9e88588dc5810f7a93875
AkosPaha <paha.akos@gmail.com>
2021-06-15 07:10:41 +0200

    optimised flags

--- app/src/main/java/de/dertyp7214/rboardthememanager/utils/Flags.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/utils/Flags.kt
@@ -12,7 +12,12 @@ import com.topjohnwu.superuser.io.SuFile
 import com.topjohnwu.superuser.io.SuFileInputStream
 import de.Maxr1998.modernpreferences.Preference
 import de.Maxr1998.modernpreferences.PreferenceScreen
-import de.Maxr1998.modernpreferences.helpers.*
+import de.Maxr1998.modernpreferences.helpers.onCheckedChange
+import de.Maxr1998.modernpreferences.helpers.onClick
+import de.Maxr1998.modernpreferences.helpers.pref
+import de.Maxr1998.modernpreferences.helpers.switch
+import de.Maxr1998.modernpreferences.preferences.CategoryHeader
+import de.Maxr1998.modernpreferences.preferences.SwitchPreference
 import de.dertyp7214.rboardthememanager.Application
 import de.dertyp7214.rboardthememanager.Config
 import de.dertyp7214.rboardthememanager.R
@@ -30,7 +35,8 @@ import kotlin.collections.HashMap
 class Flags {
     enum class FILES(val fileName: String) {
         FLAGS("flag_value.xml"),
-        GBOARD_PREFERENCES("${Config.GBOARD_PACKAGE_NAME}_preferences.xml")
+        GBOARD_PREFERENCES("${Config.GBOARD_PACKAGE_NAME}_preferences.xml"),
+        NONE("")
     }
 
     enum class TYPE {
@@ -50,9 +56,10 @@ class Flags {
         @DrawableRes val icon: Int,
         val defaultValue: Any,
         val type: TYPE,
-        val file: FILES,
+        val file: FILES = FILES.NONE,
         val valueMap: Map<Any?, Any?>? = null,
         val visible: Boolean = true,
+        val linkedKeys: List<String> = listOf(),
         val onClick: () -> Unit = {}
     ) {
         GENERAL(
@@ -61,8 +68,7 @@ class Flags {
             -1,
             -1,
             "",
-            TYPE.GROUP,
-            FILES.FLAGS
+            TYPE.GROUP
         ),
         EMOJI_COMPAT_APP_WHITELIST(
             "emoji_compat_app_whitelist",
@@ -170,9 +176,108 @@ class Flags {
             TYPE.BOOLEAN,
             FILES.GBOARD_PREFERENCES
         ),
-        ENABLE_AUTO_FLOAT_KEYBOARD_IN_MULTI_WINDOW(
-            "enable_auto_float_keyboard_in_multi_window",
-            R.string.enable_auto_float_keyboard_in_multi_window,
+
+        /* BRANDING */
+
+        BRANDING(
+            "branding",
+            R.string.show_branding_on_space,
+            R.string.show_branding_on_space,
+            R.drawable.ic_google_logo,
+            false,
+            TYPE.BOOLEAN,
+            FILES.NONE,
+            linkedKeys = listOf(
+                "show_branding_on_space",
+                "show_branding_interval_seconds",
+                "branding_fadeout_delay_ms"
+            )
+        ),
+        SHOW_BRANDING_ON_SPACE(
+            "show_branding_on_space",
+            R.string.show_branding_on_space,
+            -1,
+            -1,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            visible = false
+        ),
+        SHOW_BRANDING_INTERVAL_SECONDS(
+            "show_branding_interval_seconds",
+            R.string.show_branding_interval_seconds,
+            -1,
+            -1,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            mapOf(
+                Pair(true, 0L),
+                Pair(false, 86400000L)
+            ),
+            false
+        ),
+        BRANDING_FADEOUT_DELAY_MS(
+            "branding_fadeout_delay_ms",
+            R.string.branding_fadeout_delay_ms,
+            -1,
+            -1,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            mapOf(
+                Pair(true, Int.MAX_VALUE.toLong()),
+                Pair(false, 900L)
+            ),
+            false
+        ),
+
+        /* MONET */
+
+        MONET(
+            "monet",
+            R.string.monet,
+            R.string.monet_details,
+            R.drawable.ic_themes,
+            false,
+            TYPE.BOOLEAN,
+			visible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S,
+            linkedKeys = listOf("use_silk_theme_by_default", "silk_on_all_pixel", "silk_theme")
+        ),
+        USE_SILK_THEME_BY_DEFAULT(
+            "use_silk_theme_by_default",
+            R.string.use_silk_theme_by_default,
+            -1,
+            R.drawable.ic_themes,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            visible = false
+        ),
+        SILK_ON_ALL_PIXEL(
+            "silk_on_all_pixel",
+            R.string.silk_on_all_pixel,
+            -1,
+            R.drawable.ic_themes,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            visible = false
+        ),
+        SILK_THEME(
+            "silk_theme",
+            R.string.silk_theme,
+            -1,
+            R.drawable.ic_themes,
+            false,
+            TYPE.BOOLEAN,
+            FILES.FLAGS,
+            visible = false
+        ),
+
+        PILL_SHAPED_KEY(
+            "pill_shaped_key",
+            R.string.pill_shaped_key,
             -1,
             -1,
             false,
@@ -197,112 +302,23 @@ class Flags {
             TYPE.BOOLEAN,
             FILES.FLAGS
         ),
-        BRANDING(
-            "branding",
-            R.string.branding,
-            -1,
-            -1,
-            "",
-            TYPE.GROUP,
-            FILES.FLAGS
-        ),
-        SHOW_BRANDING_ON_SPACE(
-            "show_branding_on_space",
-            R.string.show_branding_on_space,
+        ENABLE_AUTO_FLOAT_KEYBOARD_IN_MULTI_WINDOW(
+            "enable_auto_float_keyboard_in_multi_window",
+            R.string.enable_auto_float_keyboard_in_multi_window,
             -1,
             -1,
             false,
             TYPE.BOOLEAN,
             FILES.FLAGS
         ),
-        SHOW_BRANDING_INTERVAL_SECONDS(
-            "show_branding_interval_seconds",
-            R.string.show_branding_interval_seconds,
-            -1,
-            -1,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS,
-            mapOf(
-                Pair(true, 0L),
-                Pair(false, 86400000L)
-            )
-        ),
-        BRANDING_FADEOUT_DELAY_MS(
-            "branding_fadeout_delay_ms",
-            R.string.branding_fadeout_delay_ms,
-            -1,
-            -1,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS,
-            mapOf(
-                Pair(true, Int.MAX_VALUE.toLong()),
-                Pair(false, 900L)
-            )
-        ),
-        ANDROID_12(
-            "android_12",
-            R.string.android_12,
-            -1,
-            -1,
-            "",
-            TYPE.GROUP,
-            FILES.FLAGS
-        ),
-        PILL_SHAPED_KEY(
-            "pill_shaped_key",
-            R.string.pill_shaped_key,
-            -1,
-            -1,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS
-        ),
-        MONET(
-            "monet",
-            R.string.monet,
-            -1,
-            -1,
-            "",
-            TYPE.GROUP,
-            FILES.FLAGS
-        ),
-        USE_SILK_THEME_BY_DEFAULT(
-            "use_silk_theme_by_default",
-            R.string.use_silk_theme_by_default,
-            -1,
-            R.drawable.ic_themes,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS
-        ),
-        SILK_ON_ALL_PIXEL(
-            "silk_on_all_pixel",
-            R.string.silk_on_all_pixel,
-            -1,
-            R.drawable.ic_themes,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS
-        ),
-        SILK_THEME(
-            "silk_theme",
-            R.string.silk_theme,
-            -1,
-            R.drawable.ic_themes,
-            false,
-            TYPE.BOOLEAN,
-            FILES.FLAGS
-        ),
+
         EMPTY(
             "empty",
             -1,
             -1,
             -1,
             "",
-            TYPE.GROUP,
-            FILES.FLAGS
+            TYPE.GROUP
         ),
         SHOW_ALL_FLAGS(
             "show_all_flags",
@@ -311,7 +327,7 @@ class Flags {
             -1,
             "",
             TYPE.JUST_CLICK,
-            FILES.FLAGS,
+            FILES.NONE,
             onClick = {
                 Application.context?.let {
                     PreferencesActivity::class.java.start(
@@ -324,11 +340,21 @@ class Flags {
             }
         );
 
-        inline fun <reified T> getValue(defaultValue: T? = null): T? {
-            return if (valueMap != null) valueMap.filter {
-                it.value == values[key]
-            }.entries.firstOrNull()?.key as? T ?: defaultValue else values[key] as? T
-                ?: defaultValue
+        @Suppress("UNCHECKED_CAST")
+        fun <T> getValue(defaultValue: T? = null): T? {
+            fun <E> getVal(defaultValue: E? = null, valueMap: Map<Any?, Any?>?): E? {
+                return if (valueMap != null) valueMap.filter {
+                    it.value == values[key]
+                }.entries.firstOrNull()?.key as? E ?: defaultValue else values[key] as? E
+                    ?: defaultValue
+            }
+            return if (linkedKeys.isEmpty()) getVal(defaultValue, valueMap) else {
+                !linkedKeys.map { key ->
+                    values().find { flag -> flag.key == key }?.let {
+                        it.getValue(it.defaultValue)
+                    } ?: false
+                }.contains(false) as? T ?: defaultValue
+            }
         }
 
         @SuppressLint("SdCardPath")
@@ -337,40 +363,44 @@ class Flags {
 
     fun preferences(builder: PreferenceScreen.Builder) {
         val prefs = PreferenceManager.getDefaultSharedPreferences(Application.context)
+        val preferences = hashMapOf<String, Preference>()
         FLAGS.values().forEach { item ->
-            if (item.visible) {
-                prefs.edit { remove(item.key) }
-                val pref: Preference = when (item.type) {
-                    TYPE.BOOLEAN -> builder.switch(item.key) {
-                        defaultValue = item.getValue(item.defaultValue) as? Boolean ?: false
-                        onCheckedChange {
-                            if (!item.setValue(it)) Application.context?.let { context ->
-                                Toast.makeText(
-                                    context,
-                                    R.string.error,
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
-                            true
+            prefs.edit { remove(item.key) }
+            val pref: Preference = when (item.type) {
+                TYPE.BOOLEAN -> SwitchPreference(item.key).apply {
+                    defaultValue = item.getValue(item.defaultValue) as? Boolean ?: false
+                    onCheckedChange {
+                        if (!item.setValue(it)) Application.context?.let { context ->
+                            Toast.makeText(
+                                context,
+                                R.string.error,
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    }
-                    TYPE.INT, TYPE.LONG, TYPE.FLOAT -> builder.pref(item.key) {}
-                    TYPE.GROUP -> builder.categoryHeader(item.key) {
-                        titleRes = item.title
-                        summaryRes = item.summary
-                    }.let { Preference("") }
-                    TYPE.STRING -> builder.pref(item.key) {}
-                    TYPE.JUST_CLICK -> builder.pref(item.key) {
-                        onClick { item.onClick(); false }
+                        item.linkedKeys.forEach { key ->
+                            preferences[key]?.let { preference ->
+                                if (preference is SwitchPreference) preference.checked = it
+                            }
+                        }
+                        true
                     }
                 }
-                pref.apply {
-                    titleRes = item.title
-                    summaryRes = item.summary
-                    iconRes = item.icon
-                    if (item.summary == -1) summary = item.key
+                TYPE.INT, TYPE.LONG, TYPE.FLOAT -> Preference(item.key)
+                TYPE.GROUP -> CategoryHeader(item.key)
+                TYPE.STRING -> Preference(item.key).apply {}
+                TYPE.JUST_CLICK -> Preference(item.key).apply {
+                    onClick { item.onClick(); false }
                 }
             }
+            pref.apply {
+                titleRes = item.title
+                summaryRes = item.summary
+                iconRes = item.icon
+                visible = item.visible
+                if (item.summary == -1) summary = item.key
+            }
+            preferences[item.key] = pref
+            builder.addPreferenceItem(pref)
         }
     }
 
@@ -459,6 +489,7 @@ class Flags {
 
         @SuppressLint("SdCardPath")
         private fun <T> setValue(value: T, key: String, file: FILES): Boolean {
+            if (file == FILES.NONE) return true
             val fileName = "/data/data/${Config.GBOARD_PACKAGE_NAME}/shared_prefs/${file.fileName}"
             val content = SuFileInputStream.open(SuFile(fileName)).use {
                 it.bufferedReader().readText()
