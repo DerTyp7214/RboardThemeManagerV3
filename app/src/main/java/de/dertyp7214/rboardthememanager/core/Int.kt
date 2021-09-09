2452a4e066c2ff00609c5d35405960e52460cd92
Josua Lengwenath <lengwenath1@gmail.com>
2021-09-09 15:32:13 +0200

    Missing icon/text flags fix

--- app/src/main/java/de/dertyp7214/rboardthememanager/core/Int.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/core/Int.kt
@@ -1,6 +1,7 @@
 package de.dertyp7214.rboardthememanager.core
 
 import android.graphics.Color
+import de.dertyp7214.rboardthememanager.R
 
 fun Int.changeAlpha(alpha: Int): Int {
     return Color.argb(
@@ -12,3 +13,13 @@ fun Int.changeAlpha(alpha: Int): Int {
 }
 
 fun lerp(a: Int, b: Int, f: Float): Float = a + (f * (a - b).toFloat())
+
+val Int.safeString: Int
+    get() {
+        return if (this < 1) R.string.none else this
+    }
+
+val Int.safeIcon: Int
+    get() {
+        return if (this < 1) R.drawable.empty else this
+    }
