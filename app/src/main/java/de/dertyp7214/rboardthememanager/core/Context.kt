5f677d0cafe2237811bc18a0531c759496f5f6be
AkosPaha <paha.akos@gmail.com>
2021-09-09 15:11:55 +0200

    fix bottom bar and navbar

--- app/src/main/java/de/dertyp7214/rboardthememanager/core/Context.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/core/Context.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.core.os.postDelayed
+import android.os.Build
 
 fun delayed(delay: Long, callback: () -> Unit) {
     Handler(Looper.getMainLooper()).postDelayed(delay) { callback() }
@@ -19,6 +20,9 @@ fun Context.getAttr(@AttrRes attr: Int): Int {
 
 fun Context.getNavigationBarHeight(): Int {
     val resourceId: Int = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        return 0
+    }
     return if (resourceId > 0) {
         resources.getDimensionPixelSize(resourceId)
     } else 0
