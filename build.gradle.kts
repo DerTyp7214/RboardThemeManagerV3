367b1dbe8d06d0f6f5101d5bfe3d6ec0ec05ae6c
AkosPaha <paha.akos@gmail.com>
2021-09-20 22:13:56 +0200

    icon colors, fixed themes not installing when they have space in their name, switch to Kotlin DSL, fix warnings and remove unnecessary code, use less intense colors in dark mode

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+@file:Suppress("UNUSED_VARIABLE")
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.1.0-alpha12")
+        //noinspection DifferentKotlinGradleVersion
+        classpath(kotlin("gradle-plugin", version = "1.5.30"))
+        classpath("com.google.gms:google-services:4.3.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "Rboard Theme Manager V3"
-include ':app'
+include(":app")
