88af15b444c6d282fb16e68ec02edb2e92993dec
Akos Paha <paha.akos@gmail.com>
2021-10-15 20:15:48 +0200

    Gradle Tools Update 🦤

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha13")
+        classpath("com.android.tools.build:gradle:7.1.0-beta01")
         //noinspection DifferentKotlinGradleVersion
         classpath(kotlin("gradle-plugin", version = "1.5.31"))
         classpath("com.google.gms:google-services:4.3.10")
