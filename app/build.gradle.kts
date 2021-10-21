0a91d3cd05059620951575f3727be306403daf64
Josua Lengwenath <lengwenath1@gmail.com>
2021-10-21 23:46:50 +0200

    Improved Chinese translations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 31
         targetSdk = 31
-        versionCode = 336001
+        versionCode = 336002
         versionName = "3.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
