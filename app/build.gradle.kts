781551ed7f301e602eda1221dab10e6461e8bfe6
Akos Paha <paha.akos@gmail.com>
2021-10-22 21:06:38 +0200

    Removed Staging_RCompatible branch, Updated and added some icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 26
         targetSdk = 31
-        versionCode = 336002
+        versionCode = 336003
         versionName = "3.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
