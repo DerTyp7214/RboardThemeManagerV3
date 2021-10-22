d420eed6b54a5f0b703b5e90e0cb56187605cd72
Akos Paha <paha.akos@gmail.com>
2021-10-22 15:59:55 +0200

    Improved Chinese translations
    Co-Authored-By: contingency <16105768+quantumfallout@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 26
         targetSdk = 31
-        versionCode = 336001
+        versionCode = 336002
         versionName = "3.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
