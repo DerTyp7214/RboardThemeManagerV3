2f289a174fe1752557971c975b63c61073841e35
Josua Lengwenath <lengwenath1@gmail.com>
2021-11-18 11:20:10 +0100

    Props "fix"

--- app/src/main/java/de/dertyp7214/rboardthememanager/core/String.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/core/String.kt
@@ -3,7 +3,9 @@ package de.dertyp7214.rboardthememanager.core
 import com.dertyp7214.logs.helpers.Logger
 import com.topjohnwu.superuser.Shell
 import de.dertyp7214.rboardthememanager.Application
+import de.dertyp7214.rboardthememanager.Config
 import de.dertyp7214.rboardthememanager.R
+import de.dertyp7214.rboardthememanager.utils.MagiskUtils
 import org.xml.sax.InputSource
 import java.io.StringReader
 import java.util.*
@@ -39,8 +41,18 @@ fun String.getSystemProperty(): String {
     }
 }
 
-fun String.setSystemProperty(value: String = "") =
-    "resetprop ${if (value.isEmpty()) "--delete " else ""}$this $value".runAsCommand()
+fun String.setSystemProperty(value: String = "", saveToModule: Boolean = false): Boolean {
+    if (saveToModule) {
+        val files = mapOf(
+            Pair(
+                "system.prop",
+                "$this=$value"
+            )
+        )
+        MagiskUtils.updateModule(Config.MODULE_META, files)
+    }
+    return "resetprop ${if (value.isEmpty()) "--delete " else ""}$this $value".runAsCommand()
+}
 
 fun List<String>.runAsCommand(callback: (result: Array<String>) -> Unit = {}): Boolean {
     return Shell.su(*this.toTypedArray()).exec().apply {
