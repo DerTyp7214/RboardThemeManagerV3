544eabbd05ab14d7f91b2a0dd388c5651caaa542
DerBot7214 <lengwenath1@gmail.com>
2022-06-20 17:00:18 +0200

    cmake

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":mathc"))
     implementation(project(":colorutilsc"))
-    //implementation(project(":composecomponents"))
     implementation(platform("com.google.firebase:firebase-bom:30.1.0"))
     implementation("com.google.firebase:firebase-messaging-ktx:23.0.5")
     implementation("com.google.firebase:firebase-analytics-ktx:21.0.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,3 @@ rootProject.name = "Rboard Theme Manager V3"
 include(":app")
 include(":mathc")
 include(":colorutilsc")
-include(":composecomponents")
