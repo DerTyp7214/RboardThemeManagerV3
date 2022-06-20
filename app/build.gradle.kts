767c5f119595f84d514331c835bf97c237335993
AkosPaha <paha.akos@gmail.com>
2022-06-20 20:56:48 +0200

    Mid-June Dependencies Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-val libsuVersion = "5.0.1"
+val libsuVersion = "5.0.2"
 val kotlinVersion: String = project.getKotlinPluginVersion()
 
 android {
@@ -72,30 +72,30 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx:21.0.0")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0-rc01")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.0-rc01")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.0-rc02")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.0-rc02")
     implementation("org.apache.commons:commons-text:1.9")
 
     implementation("com.github.topjohnwu.libsu:core:$libsuVersion")
     implementation("com.github.topjohnwu.libsu:io:$libsuVersion")
     implementation("com.github.topjohnwu.libsu:nio:$libsuVersion")
     implementation("dev.chrisbanes.insetter:insetter:0.6.1")
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0-alpha05")
     //noinspection DifferentStdlibGradleVersion
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.0")
-    implementation("androidx.core:core:1.8.0")
+    implementation("androidx.core:core:1.9.0-alpha05")
 
     implementation("com.google.android.material:material:1.7.0-alpha02")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha02")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.activity:activity-ktx:1.5.0-rc01")
+    implementation("androidx.activity:activity-ktx:1.6.0-alpha05")
     implementation("androidx.fragment:fragment-ktx:1.5.0-rc01")
     implementation("com.jaredrummler:android-shell:1.0.0")
     implementation("com.google.firebase:firebase-analytics:21.0.0")
     implementation("com.google.firebase:firebase-messaging:23.0.5")
     implementation("com.google.code.gson:gson:2.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0-rc02")
     implementation("com.bignerdranch.android:simple-item-decoration:1.0.0")
     implementation("de.dertyp7214:PRDownloader:v0.6.0")
     implementation("com.github.skydoves:balloon:1.4.5")
@@ -109,7 +109,7 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-beta03")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-rc01")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.0")
 
 
