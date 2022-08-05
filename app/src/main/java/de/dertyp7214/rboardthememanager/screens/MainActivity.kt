d2909e1573b97757af29a805e720a7c014b38a48
DerBot7214 <lengwenath1@gmail.com>
2022-08-05 23:50:27 +0200

    lib updates, open only patcher or play store when pressing patch

--- app/src/main/java/de/dertyp7214/rboardthememanager/screens/MainActivity.kt
+++ app/src/main/java/de/dertyp7214/rboardthememanager/screens/MainActivity.kt
@@ -52,6 +52,8 @@ import com.topjohnwu.superuser.io.SuFile
 import de.dertyp7214.rboardthememanager.BuildConfig
 import de.dertyp7214.rboardthememanager.Config
 import de.dertyp7214.rboardthememanager.Config.MODULE_META
+import de.dertyp7214.rboardthememanager.Config.PATCHER_PACKAGE
+import de.dertyp7214.rboardthememanager.Config.PLAY_URL
 import de.dertyp7214.rboardthememanager.R
 import de.dertyp7214.rboardthememanager.adapter.MenuAdapter
 import de.dertyp7214.rboardthememanager.core.*
@@ -468,15 +470,24 @@ class MainActivity : AppCompatActivity() {
                                             R.drawable.ic_patch,
                                             R.string.patch
                                         ) {
-                                            val themeFile = File(theme.path)
-                                            val imageFile = theme.image?.let { File(theme.path.removeSuffix(".zip")) }
+                                            if (PackageUtils.isPackageInstalled(PATCHER_PACKAGE, packageManager)) {
+                                                val themeFile = File(theme.path)
+                                                val imageFile = theme.image?.let {
+                                                    File(
+                                                        theme.path.removeSuffix(".zip")
+                                                    )
+                                                }
 
-                                            val files = arrayListOf(themeFile)
-                                            if (imageFile != null) files.add(imageFile)
+                                                val files = arrayListOf(themeFile)
+                                                if (imageFile != null) files.add(imageFile)
 
-                                            val pack = File(cacheDir, "export.pack")
-                                            ZipHelper().zip(files.map { it.absolutePath }, pack.absolutePath)
-                                            ThemeUtils.shareTheme(this, pack, true)
+                                                val pack = File(cacheDir, "export.pack")
+                                                ZipHelper().zip(
+                                                    files.map { it.absolutePath },
+                                                    pack.absolutePath
+                                                )
+                                                ThemeUtils.shareTheme(this, pack, true, PATCHER_PACKAGE)
+                                            } else openUrl(PLAY_URL(PATCHER_PACKAGE))
                                         }
                                     )
                                     menuItems.add(
