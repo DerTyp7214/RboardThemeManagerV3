6a54b87d959673db1c8e3b5fafaded37413ee73b
DerTyp7214 <lengwenath1@gmail.com>
2022-09-15 01:23:17 +0200

    lib updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("com.google.gms.google-services")
 }
 
-val libsuVersion = "5.0.2"
+val libsuVersion = "5.0.3"
 val kotlinVersion: String = project.getKotlinPluginVersion()
 
 @Suppress("UnstableApiUsage")
@@ -24,8 +24,8 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 31
         targetSdk = 33
-        versionCode = 366000
-        versionName = "3.6.6"
+        versionCode = 367000
+        versionName = "3.6.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -81,34 +81,34 @@ dependencies {
     implementation("com.google.protobuf:protobuf-kotlin:3.21.5")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.2")
     implementation("org.apache.commons:commons-text:1.9")
 
     implementation("com.github.topjohnwu.libsu:core:$libsuVersion")
     implementation("com.github.topjohnwu.libsu:io:$libsuVersion")
     implementation("com.github.topjohnwu.libsu:nio:$libsuVersion")
     implementation("dev.chrisbanes.insetter:insetter:0.6.1")
-    implementation("androidx.core:core-ktx:1.9.0-rc01")
+    implementation("androidx.core:core-ktx:1.9.0")
     //noinspection DifferentStdlibGradleVersion
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.10")
-    implementation("androidx.core:core:1.9.0-rc01")
+    implementation("androidx.core:core:1.9.0")
 
     implementation("com.google.android.material:material:1.8.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha03")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.activity:activity-ktx:1.6.0-rc01")
+    implementation("androidx.activity:activity-ktx:1.6.0-rc02")
     implementation("androidx.fragment:fragment-ktx:1.5.2")
     implementation("com.jaredrummler:android-shell:1.0.0")
     implementation("com.google.firebase:firebase-analytics:21.1.1")
     implementation("com.google.firebase:firebase-messaging:23.0.8")
     implementation("com.google.code.gson:gson:2.9.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0-alpha01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0-alpha02")
     implementation("com.bignerdranch.android:simple-item-decoration:1.0.0")
     implementation("de.dertyp7214:PRDownloader:v0.6.0")
     implementation("com.github.skydoves:balloon:1.4.7")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.4-alpha07")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0-alpha07")
@@ -118,7 +118,7 @@ dependencies {
     implementation("com.github.murgupluoglu:flagkit-android:1.0.2")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0-beta01")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.0-beta02")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.10")
 
     implementation("com.google.android.play:core:1.10.3")
