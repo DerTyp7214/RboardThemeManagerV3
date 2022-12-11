9bb189c1e6e9d93c445a1b86a938161611f04baa
AkosPaha <paha.akos@gmail.com>
2022-12-11 11:07:02 +0100

    Fix download count reset after update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 23
         targetSdk = 33
-        versionCode = 371000
-        versionName = "3.7.1"
+        versionCode = 372000
+        versionName = "3.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -73,13 +73,13 @@ dependencies {
     implementation(project(":mathc"))
     implementation(project(":rboardcomponents"))
 
-    implementation(platform("com.google.firebase:firebase-bom:31.1.0"))
-    implementation("com.google.firebase:firebase-messaging-ktx:23.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:31.1.1"))
+    implementation("com.google.firebase:firebase-messaging-ktx:23.1.1")
     implementation("com.google.firebase:firebase-analytics-ktx:21.2.0")
 
     implementation("com.github.DerTyp7214:PreferencesPlus:1.0")
 
-    implementation("com.google.protobuf:protobuf-kotlin:3.21.10")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.11")
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0-alpha04")
@@ -94,14 +94,14 @@ dependencies {
     //noinspection DifferentStdlibGradleVersion
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.21")
     implementation("androidx.core:core:1.9.0")
-    implementation("com.google.android.material:material:1.8.0-alpha03")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha04")
+    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha05")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.activity:activity-ktx:1.7.0-alpha02")
-    implementation("androidx.fragment:fragment-ktx:1.6.0-alpha03")
+    implementation("androidx.fragment:fragment-ktx:1.6.0-alpha04")
     implementation("com.jaredrummler:android-shell:1.0.0")
     implementation("com.google.firebase:firebase-analytics:21.2.0")
-    implementation("com.google.firebase:firebase-messaging:23.1.0")
+    implementation("com.google.firebase:firebase-messaging:23.1.1")
     implementation("com.google.code.gson:gson:2.10")
     implementation("com.bignerdranch.android:simple-item-decoration:1.0.0")
     implementation("de.dertyp7214:PRDownloader:v0.6.0")
@@ -116,6 +116,6 @@ dependencies {
     implementation("com.github.murgupluoglu:flagkit-android:1.0.2")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha02")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha03")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
 }
