251f3d26a1c7c466f0c930b09659d0e309b2d9b1
DerBot7214 <lengwenath1@gmail.com>
2023-01-15 20:04:28 +0100

    lib updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 31
         targetSdk = 33
-        versionCode = 375000
-        versionName = "3.7.5"
+        versionCode = 376000
+        versionName = "3.7.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -97,7 +97,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.21")
     implementation("androidx.core:core:1.9.0")
 
-    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("com.google.android.material:material:1.8.0-rc01")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha05")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.activity:activity-ktx:1.7.0-alpha02")
@@ -106,21 +106,21 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics:21.2.0")
     implementation("com.google.firebase:firebase-messaging:23.1.1")
     implementation("com.google.code.gson:gson:2.10")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0-alpha03")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0-alpha04")
     implementation("com.bignerdranch.android:simple-item-decoration:1.0.0")
     implementation("de.dertyp7214:PRDownloader:v0.6.0")
     implementation("com.github.skydoves:balloon:1.4.7")
     implementation("androidx.appcompat:appcompat:1.7.0-alpha01")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.browser:browser:1.4.0")
     implementation("com.github.bumptech.glide:glide:4.14.2")
     kapt("com.github.bumptech.glide:compiler:4.14.2")
     implementation("com.github.murgupluoglu:flagkit-android:1.0.2")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha03")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0-alpha04")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
 
     implementation("com.google.android.play:core:1.10.3")
