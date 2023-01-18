7abdbd5d33a64017216f7268a6cf4d6909f18bb5
DerBot7214 <lengwenath1@gmail.com>
2023-01-18 11:42:19 +0100

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.0.0-alpha11")
+        classpath("com.android.tools.build:gradle:8.1.0-alpha01")
         //noinspection DifferentKotlinGradleVersion
         classpath(kotlin("gradle-plugin", version = "1.8.0"))
         classpath("com.google.gms:google-services:4.3.14")
