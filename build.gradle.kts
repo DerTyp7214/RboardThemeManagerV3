057a6feb31c1f6f29c59365201a767a8a395d807
AkosPaha <paha.akos@gmail.com>
2023-01-31 11:16:44 +0100

    Add .pack.zip

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ buildscript {
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0-alpha01")
+        classpath("com.android.tools.build:gradle:8.1.0-alpha02")
         //noinspection DifferentKotlinGradleVersion
         classpath(kotlin("gradle-plugin", version = "1.8.0"))
-        classpath("com.google.gms:google-services:4.3.14")
+        classpath("com.google.gms:google-services:4.3.15")
         //classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.1")
     }
 }
