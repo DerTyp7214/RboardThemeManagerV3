2eb991a8361ff588e7f1ef3ff5dd3fa520ee4770
DerBot7214 <lengwenath1@gmail.com>
2023-04-26 12:38:50 +0200

    lib updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,9 +82,9 @@ dependencies {
     implementation(project(":colorutilsc"))
     implementation(project(":rboardcomponents"))
 
-    implementation(platform("com.google.firebase:firebase-bom:31.3.0"))
+    implementation(platform("com.google.firebase:firebase-bom:31.5.0"))
     implementation("com.google.firebase:firebase-messaging-ktx:23.1.2")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.2.1")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.2.2")
 
     implementation("com.github.os72:protobuf-dynamic:1.0.1")
 
@@ -97,25 +97,25 @@ dependencies {
     implementation("com.github.topjohnwu.libsu:io:$libsuVersion")
     implementation("com.github.topjohnwu.libsu:nio:$libsuVersion")
     implementation("dev.chrisbanes.insetter:insetter:0.6.1")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0-alpha03")
     //noinspection DifferentStdlibGradleVersion
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-    implementation("androidx.core:core:1.9.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
+    implementation("androidx.core:core:1.12.0-alpha03")
 
-    implementation("com.google.android.material:material:1.9.0-beta01")
+    implementation("com.google.android.material:material:1.10.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0-alpha09")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.activity:activity-ktx:1.8.0-alpha02")
-    implementation("androidx.fragment:fragment-ktx:1.5.6")
+    implementation("androidx.activity:activity-ktx:1.8.0-alpha03")
+    implementation("androidx.fragment:fragment-ktx:1.5.7")
     implementation("com.jaredrummler:android-shell:1.0.0")
-    implementation("com.google.firebase:firebase-analytics:21.2.1")
+    implementation("com.google.firebase:firebase-analytics:21.2.2")
     implementation("com.google.firebase:firebase-messaging:23.1.2")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("com.bignerdranch.android:simple-item-decoration:1.0.0")
     implementation("de.dertyp7214:PRDownloader:v0.6.0")
-    implementation("com.github.skydoves:balloon:1.5.2")
+    implementation("com.github.skydoves:balloon:1.5.3")
     implementation("androidx.appcompat:appcompat:1.7.0-alpha02")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -125,8 +125,8 @@ dependencies {
     implementation("com.github.murgupluoglu:flagkit-android:1.0.2")
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0-alpha01")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0-alpha03")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.21")
 
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
