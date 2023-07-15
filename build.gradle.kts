0d6ba7b88603681ad838f426db8f6a11b7b917ce
PahaAkos <paha.akos@gmail.com>
2023-07-15 22:33:31 +0200

    App & Java Version Update
    New changes:
    • Bump App Version to 3.8.4
    • Update Java Version to 20
    • Move com.google.devtools.ksp to libs.versions.toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UNUSED_VARIABLE")
-
+plugins {
+    alias(libs.plugins.ksp) apply false
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -15,7 +17,6 @@ buildscript {
         //noinspection DifferentKotlinGradleVersion
         classpath(kotlin("gradle-plugin", version = "1.9.0"))
         classpath(libs.google.services)
-        //classpath(libs.protobuf)
     }
 }
 
@@ -27,11 +28,5 @@ allprojects {
         maven("https://maven.fabric.io/public")
         maven("https://maven.google.com")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(layout.buildDirectory)
     }
 }
