899931fd0ad2c42ceae160c1e847e0dee16ef1c1
DerBot7214 <lengwenath1@gmail.com>
2023-09-05 17:31:41 +0200

    fix for #396 1st: c325d83bde4899bfbf1cf4157a1299fe4ce62064 2nd: 65d16bf33b1a72e3ea5956dd16a041d3b18cdc44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "de.dertyp7214.rboardthememanager"
         minSdk = 31
         targetSdk = 34
-        versionCode = 385000
+        versionCode = 385001
         versionName = "3.8.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
