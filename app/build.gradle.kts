eac62aa9fa2833d1ec51cd86abecffbaf78a3f01
PahaAkos <paha.akos@gmail.com>
2024-02-23 13:31:42 +0100

    Update Dependencies
    New dependencies changes:
    • Update androidx.browser:browser to 1.8.0-rc01
    • Update androidx.navigation:navigation-fragment-(ktx) & ui-(ktx) to 2.8.0-alpha03
    • Update androidx.lifecycle:lifecycle-viewmodel-ktx to 2.8.0-alpha02
    • Update androidx.compose.ui:ui-tooling to 1.7.0-alpha03
    • Update com.android.tools.build:gradle to 8.4.0-alpha11
    
    Others Changes:
    • Update versionCode to 392002 (3.9.2)
    • Update Gradle to 8.7-rc-1
    
    Signed-off-by: PahaAkos <paha.akos@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         // Update the minSdk if old Android Versions are no longer supported on the Gboard side.
         minSdk = 23
         targetSdk = 34
-        versionCode = 392001
+        versionCode = 392002
         versionName = "3.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
