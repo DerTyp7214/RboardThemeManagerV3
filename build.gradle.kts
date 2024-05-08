58c25f4f74f2d018f229619eca35a4f5c9bc9532
PahaAkos <paha.akos@gmail.com>
2024-05-08 22:37:49 +0200

    Debug Release 3.9.2 (392008)
    🔧 New dependencies changes:
    • Update Appcompat to 1.7.0-beta01
    • Update Core(-ktx) to 1.13.1
    • Update Gradle to 8.5.0-alpha08
    • Update Fragment(-ktx) to 1.7.0
    • Update Firebase-bom to 33.0.0
    • Update Firebase-analytics(-ktx) to 22.0.0
    • Update Firebase-messaging(-ktx) to 24.0.0
    • Update Kotlin to 1.9.24
    • Update Material to 1.12.0
    • Update Ui-tooling to 1.6.7
    
    🗂️ Others Changes:
    • Update versionCode to 392008 (3.9.2)
    • Update Submodules
    • Add Flags Description

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     dependencies {
         classpath(libs.gradle)
         //noinspection DifferentKotlinGradleVersion
-        classpath(kotlin("gradle-plugin", version = "1.9.23"))
+        classpath(kotlin("gradle-plugin", version = "1.9.24"))
         classpath(libs.google.services)
     }
 }
