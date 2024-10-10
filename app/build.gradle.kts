087a2953cda560d30c4c77d25d180ed37ab20772
PahaAkos <paha.akos@gmail.com>
2024-10-10 22:08:36 +0200

    🪲 Debug Release 3.9.3 (393008)
    • Bump versionCode to 393008 (3.9.3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         // Update the minSdk if old Android Versions are no longer supported on the Gboard side.
         minSdk = 23
         targetSdk = 35
-        versionCode = 393007
+        versionCode = 393008
         versionName = "3.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
