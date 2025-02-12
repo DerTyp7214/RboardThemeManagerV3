0fc994b4dc65733c5253a7cb8efeb3d4cb732557
PahaAkos <paha.akos@gmail.com>
2025-02-12 12:07:40 +0100

    Add the option to reset the system automatic themes
    • Update Dependencies
    • Update Submodules

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     dependencies {
         classpath(libs.gradle)
         //noinspection DifferentKotlinGradleVersion
-        classpath(kotlin("gradle-plugin", version = "2.1.0"))
+        classpath(kotlin("gradle-plugin", version = "2.1.10"))
         classpath(libs.google.services)
     }
 }
