5e99b4754eed10a8f5cab498ad4b485b4db501d4
PahaAkos <paha.akos@gmail.com>
2025-07-16 20:59:57 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 36
     buildToolsVersion = "36.0.0"
     buildFeatures.dataBinding = true
-    ndkVersion = "28.1.13356709"
+    ndkVersion = "28.2.13676358"
     buildFeatures.viewBinding = true
     buildFeatures.buildConfig = true
 
